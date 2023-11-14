0ec4a11647442fb735cbdb7220695ccb720fadfb
Iris Morales <moralesortegairis@gmail.com>
2023-11-14 12:13:34 +0100

    Conexión apiMovies exitosa yujuuu!!

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -9,11 +9,76 @@ use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 use App\Entity\Movie;
   
 #[Route('/api', name: 'api_')]
 class ProjectController extends AbstractController
 {
+
+  #[Route(path: '/home', name: 'homepage')]
+    public function homepage(HttpClientInterface $httpClient): Response
+    {
+        $apiKey = ENV['API_KEY'];
+        $apiUrl = "https://api.themoviedb.org/3/movie/popular?api_key={$apiKey}&language=en-US&page=1";
+
+        try {
+            $response = $httpClient->request('GET', $apiUrl, [
+                'query' => [
+                    'api_key' => $apiKey,
+                    'language' => 'en-US',
+                ],
+            ]);
+
+            $statusCode = $response->getStatusCode();
+            $content = $response->toArray();
+            
+            // Verificar si la solicitud fue exitosa (código 2xx)
+            if ($statusCode >= 200 && $statusCode < 300) {
+                $moviesData = $content['results'] ?? [];
+                
+                return $this->render('project/homepage.html.twig', [
+                    'title' => 'Popular Movies',
+                    'movies' => $moviesData,
+                ]);
+            } else {
+                // Manejar el error de acuerdo a tus necesidades
+                // Aquí puedes lanzar una excepción o mostrar un mensaje de error
+                throw new \Exception('Error en la solicitud a la API: ' . $statusCode);
+            }
+        } catch (\Exception $e) {
+            // Manejar cualquier excepción que pueda ocurrir durante la solicitud
+            // Aquí puedes registrar el error, mostrar un mensaje amigable, etc.
+            return new Response('Error: ' . $e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+/*       #[Route(path:"/home", name:"homepage")]
+  public function homepage() : Response
+  {
+    $apiKey = ENV['API_KEY']; 
+    
+    $apiUrl = "https://api.themoviedb.org/3/movie/popular?api_key={$apiKey}&language=en-US&page=1";
+    $httpClient = HttpClient::create();
+    $response = $httpClient->request('GET', $apiUrl, [
+                'query' => [
+                    'api_key' => $apiKey,
+                    'language' => 'en-US',
+                ],
+                ]);
+    $responseData = $response->toArray();
+        // Obtener la lista de películas desde la respuesta de la API
+        $moviesData = isset($responseData['results']) ? $responseData['results'] : [];
+        return $this->render('project/homepage.html.twig', [
+            'title' => 'Popular Movies',
+            'movies' => $moviesData,
+        ]);
+    } */
+
+
+
+
     #[Route('/movies', name: 'movies_index', methods:['get'] )]
     public function index(ManagerRegistry $doctrine): JsonResponse
     {
@@ -77,27 +142,4 @@ class ProjectController extends AbstractController
             
         return $this->json($data);
     }
-
-    #[Route(path:"/apiMovies", name:"homepage")]
-  public function homepage() : Response
-  {
-    $apiKey = ENV['API_KEY']; 
-    
-    $apiUrl = "https://api.themoviedb.org/3/movie/popular?api_key={$apiKey}&language=en-US&page=1";
-    $httpClient = HttpClient::create();
-    $response = $httpClient->request('GET', $apiUrl, [
-                'query' => [
-                    'api_key' => $apiKey,
-                    'language' => 'en-US',
-                ],
-                ]);
-    $responseData = $response->toArray();
-        // Obtener la lista de películas desde la respuesta de la API
-        $moviesData = isset($responseData['results']) ? $responseData['results'] : [];
-        return $this->render('home/homepage.html.twig', [
-            'title' => 'Popular Movies',
-            'movies' => $moviesData,
-        ]);
-    }
-
 }
